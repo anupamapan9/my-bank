07c8e73c77562ebec70d7bfa065eb7f931cccc4c
anupamapan9 <anupamapan0905@gmail.com>
2022-02-09 23:38:24 +0600

    first commit

---
+++ js/scritpt.js
@@ -0,0 +1,13 @@
+document.getElementById('login-submit').addEventListener("click", function () {
+    const emailField = document.getElementById("user-email");
+    const userEmail = emailField.value;
+    const passwordField = document.getElementById("user-password");
+    const userPasssword = passwordField.value;
+    if (userEmail == '01755114282' && userPasssword == '01755114282') {
+        window.location.href = 'banking.html';
+    } else {
+        alert('thik Password de')
+    }
+
+
+})
