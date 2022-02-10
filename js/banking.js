2c8e2f28850b90c578d5a7b823c23a6389a4a2a3
anupamapan9 <anupamapan0905@gmail.com>
2022-02-10 15:07:30 +0600

    deposite & balance function added

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,31 @@
 
-const dipositeButton = document.getElementById("diposite-button").addEventListener('click', function () {
-    console.log(8)
+document.getElementById("diposite-button").addEventListener('click', function () {
+
+
+    // deposite functions***********************
+    // new deposite by a user 
+
+    const depositeInput = document.getElementById("deposit-amount")
+    const depositAmount = parseFloat(depositeInput.value);
+
+    // user's current deposite 
+    const depositeTotal = document.getElementById("total-diposite");
+    const totalDepositeValue = parseFloat(depositeTotal.innerText);
+
+    // after sum deposite 
+    const newAmount = totalDepositeValue + depositAmount;
+    console.log(newAmount);
+    depositeTotal.innerText = newAmount;
+
+    // balnace function ********************
+
+    const userBalance = document.getElementById("user-balance")
+    const totalBalance = parseFloat(userBalance.innerText);
+
+    const userNewBalance = depositAmount + totalBalance;
+    console.log(userNewBalance);
+    userBalance.innerText = userNewBalance;
+
+    depositeInput.value = "";
+
 })
