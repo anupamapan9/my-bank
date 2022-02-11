90285f05b7b31c18fac2ff28146b66410688dfc2
anupamapan9 <anupamapan0905@gmail.com>
2022-02-11 18:02:09 +0600

    error handeler added

--- js/scritpt.js
+++ js/scritpt.js
@@ -3,7 +3,7 @@ document.getElementById('login-submit').addEventListener("click", function () {
     const userEmail = emailField.value;
     const passwordField = document.getElementById("user-password");
     const userPasssword = passwordField.value;
-    if (userEmail == '01755114282' && userPasssword == '01755114282') {
+    if (userEmail == 'anupam' && userPasssword == 'anupam') {
         window.location.href = 'banking.html';
     } else {
         alert('thik Password de')
