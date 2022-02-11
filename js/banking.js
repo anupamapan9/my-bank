101728d353da9c5cb5e64a7a6fa04d6a62772a4d
anupamapan9 <anupamapan0905@gmail.com>
2022-02-11 17:59:49 +0600

    error handeler added

--- js/banking.js
+++ js/banking.js
@@ -10,50 +10,51 @@ function updateTotal(id, amount) {
     const depositeTotal = document.getElementById(id);
     const totalDepositeValue = parseFloat(depositeTotal.innerText);
     const newAmount = totalDepositeValue + amount;
+    depositeTotal.innerText = newAmount;
     return newAmount;
+}
+function currentBlance() {
+    const userBalance = document.getElementById("user-balance")
+    const balancText = userBalance.innerText;
+    const balanceValue = parseFloat(balancText);
+    return balanceValue;
+}
+
+function updateBalance(amount, isAdded) {
+    const userBalance = document.getElementById("user-balance")
+    const balanceValue = currentBlance()
+    if (isAdded == true) {
+        const userNewBalance = amount + balanceValue;
+        userBalance.innerText = userNewBalance;
+    } else {
+        const userNewBalance = balanceValue - amount;
+        userBalance.innerText = userNewBalance;
+    }
 }
 
 
 // Deposite button handels *************
 document.getElementById("diposite-button").addEventListener('click', function () {
-    // deposite functions***********************
     const depositAmount = getInputValue("deposit-amount");
+    if (depositAmount > 0) {
+        const newAmount = updateTotal("total-diposite", depositAmount);
+        const userBalance = updateBalance(newAmount, true);
+    } else {
+        alert("Enter some positive number")
+    }
 
-    // user's current deposite 
-    /*
-     const depositeTotal = document.getElementById("total-diposite");
-    const totalDepositeValue = parseFloat(depositeTotal.innerText);
-    const newAmount = totalDepositeValue + depositAmount;
-     */
-    const depositeTotal = document.getElementById("total-diposite")
-    const newAmount = updateTotal("total-diposite", depositAmount);
-    depositeTotal.innerText = newAmount;
-
-    // balnace function ********************
-
-    const userBalance = document.getElementById("user-balance")
-    const totalBalancText = userBalance.innerText;
-    const totalBalanceValue = parseFloat(totalBalancText);
-    const userNewBalance = depositAmount + totalBalanceValue;
-    userBalance.innerText = userNewBalance;
 })
 
 // withdraw button handel 
 document.getElementById("withdraw-button").addEventListener('click', function () {
     //    withdraw balance come 
     const withdarwInputValue = getInputValue("withdraw-amount");
-    const newTotalWithdarw = updateTotal("total-withdaw", withdarwInputValue);
-    const withdrawTotal = document.getElementById("total-withdaw")
-    withdrawTotal.innerText = newTotalWithdarw;
-
-    // balance update *******************
-    const currentBalance = document.getElementById("user-balance");
-    const currentBalanceText = currentBalance.innerText;
-    const currentBalanceValue = parseFloat(currentBalanceText);
-
-    const newBalance = currentBalanceValue - withdarwInputValue;
-    currentBalance.innerHTML = newBalance;
-
-
-    console.log(currentBalanceValue)
+    const balanceValue = currentBlance()
+    if (withdarwInputValue > 0 && balanceValue > withdarwInputValue) {
+        const newTotalWithdarw = updateTotal("total-withdaw", withdarwInputValue);
+        const userBalance = updateBalance(newTotalWithdarw, false);
+    }
+    if (balanceValue < withdarwInputValue) {
+        alert('Fokinni!!! Bank a ato taka nai')
+    }
 })
